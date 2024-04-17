e2f8ace28b19b97b47894a5a777519eb05b8dff7
jonas <jonasmichael2021@outlook.com>
2024-04-17 17:31:52 -0300

    requisito 9

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,14 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(self) -> str:
+        simple_report = super().generate()
+        complete_report = "Stocked products by company:\n"
+        complete_report += "\n".join(
+            [
+                f"{company}: {value}"
+                for company, value in self.companies_inventories.items()
+            ]
+        )
+        return simple_report + complete_report + "\n"
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,5 +1,5 @@
+
 from datetime import date, datetime
-from typing import Dict
 
 from inventory_report.inventory import Inventory
 
@@ -8,31 +8,30 @@ class SimpleReport:
 
     def __init__(self) -> None:
         self._inventory_list: list[Inventory] = []
+        self._companies_inventories: dict[str, int] = {}
 
     def add_inventory(self, inventory: Inventory) -> None:
+        """Function printing python version."""
         self._inventory_list.append(inventory)
 
     def generate(self) -> str:
         products = [
             product
-            for inventory in self._inventory_list
+            for inventory in self.inventory_list
             if inventory.data
             for product in inventory.data
         ]
 
         oldest_manufacturing_date = date.today()
         closest_expiration_date = date(9999, 12, 31)
-        companies_inventories: Dict[str, int] = {}
 
         for product in products:
             manufacturing_date = datetime.strptime(
                 product.manufacturing_date, "%Y-%m-%d"
             ).date()
-
             expiration_date = datetime.strptime(
                 product.expiration_date, "%Y-%m-%d"
             ).date()
-
             company_name = product.company_name
 
             if manufacturing_date < oldest_manufacturing_date:
@@ -44,13 +43,13 @@ class SimpleReport:
             ):
                 closest_expiration_date = expiration_date
 
-            companies_inventories[company_name] = (
-                companies_inventories.get(company_name, 0) + 1
+            self._companies_inventories[company_name] = (
+                self._companies_inventories.get(company_name, 0) + 1
             )
 
         company_largest_inventory = max(
-            companies_inventories,
-            key=companies_inventories.get,
+            self._companies_inventories,
+            key=self._companies_inventories.get,
         )
 
         return (
@@ -63,3 +62,7 @@ class SimpleReport:
     @property
     def inventory_list(self) -> list[Inventory]:
         return self._inventory_list
+
+    @property
+    def companies_inventories(self) -> dict[str, int]:
+        return self._companies_inventories
