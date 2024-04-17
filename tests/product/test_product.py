3495a4655da2c4b84db252515e2ef2309ddaa574
jonas <jonasmichael2021@outlook.com>
2024-04-17 14:55:29 -0300

    requisito 1?

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,23 +2,17 @@ from inventory_report.product import Product
 
 
 def test_create_product() -> None:
-    product = Product(
-        "1",
-        "aptamil",
-        "Danone",
-        "29/11/2023",
-        "30/05/2025",
-        "1821318274",
-        "Fórmula infantil de seguimento para lactentes",
-    )
+    product = {
+        "id": "1",
+        "product_name": "Leite",
+        "company_name": "Parmalat",
+        "manufacturing_date": "10/01/2024",
+        "expiration_date": "10/02/2024",
+        "serial_number": "1821318274",
+        "storage_instructions": "Guardar em lugar ventilado e longe do sol",
+    }
 
-    assert product.id == "1"
-    assert product.product_name == "aptamil"
-    assert product.company_name == "Danone"
-    assert product.manufacturing_date == "29/11/2023"
-    assert product.expiration_date == "10/02/2024"
-    assert product.serial_number == "1821318274"
-    assert (
-        product.storage_instructions
-        == "Fórmula infantil de seguimento para lactentes"
-    )
+    product = Product(**product)
+
+    for key, value in product.items():
+        assert getattr(product, key) == value
--- tests/product_report/test_product_report.py
+++
@@ -1,2 +0,0 @@
-def test_product_report() -> None:
-    raise NotImplementedError
