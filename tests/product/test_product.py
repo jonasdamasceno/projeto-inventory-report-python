810a64b197b4f844a030c4a2b0b72bacfb2255a5
jonas <jonasmichael2021@outlook.com>
2024-04-17 15:05:13 -0300

    bakup

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,17 +2,23 @@ from inventory_report.product import Product
 
 
 def test_create_product() -> None:
-    product = {
-        "id": "1",
-        "product_name": "Leite",
-        "company_name": "Parmalat",
-        "manufacturing_date": "10/01/2024",
-        "expiration_date": "10/02/2024",
-        "serial_number": "1821318274",
-        "storage_instructions": "Guardar em lugar ventilado e longe do sol",
-    }
+    product = Product(
+        "1",
+        "Leite",
+        "Parmalat",
+        "10/01/2024",
+        "10/02/2024",
+        "1821318274",
+        "Guardar em lugar ventilado e longe do sol",
+    )
 
-    product = Product(**product)
-
-    for key, value in product.items():
-        assert getattr(product, key) == value
+    assert product.id == "1"
+    assert product.product_name == "Leite"
+    assert product.company_name == "Parmalat"
+    assert product.manufacturing_date == "10/01/2024"
+    assert product.expiration_date == "10/02/2024"
+    assert product.serial_number == "1821318274"
+    assert (
+        product.storage_instructions
+        == "Guardar em lugar ventilado e longe do sol"
+    )
