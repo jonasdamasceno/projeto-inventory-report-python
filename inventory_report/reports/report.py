3e26a4c3ef33cf1a7cfbfefea9209c8357f83b2f
jonas <jonasmichael2021@outlook.com>
2024-04-17 17:07:25 -0300

    requsito 6

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,9 @@
-class Report:
-    pass
+from typing import Protocol
+
+
+class Report(Protocol):
+    def add_inventory(self, inventory) -> None:
+        pass
+
+    def generate(self) -> str:
+        pass
