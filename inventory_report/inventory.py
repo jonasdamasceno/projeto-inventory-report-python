3c0fe3daf8ff5cf1cef853545ae5e933e711005a
jonas <jonasmichael2021@outlook.com>
2024-04-17 16:47:36 -0300

    requisito 55

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from typing import List
+
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: List[Product] | None = None) -> None:
+        self._data: List[Product] = data if data else []
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data
+
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
