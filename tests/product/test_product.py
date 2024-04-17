d25ed991a8d9c507f1e8e4d097e9bd4d89e7d318
jonas <jonasmichael2021@outlook.com>
2024-04-17 14:33:56 -0300

    requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -4,21 +4,21 @@ from inventory_report.product import Product
 def test_create_product() -> None:
     product = Product(
         "1",
-        "Leite",
-        "Parmalat",
-        "10/01/2024",
-        "10/02/2024",
+        "aptamil",
+        "Danone",
+        "29/11/2023",
+        "30/05/2025",
         "1821318274",
-        "Guardar em lugar ventilado e longe do sol",
+        "Fórmula infantil de seguimento para lactentes",
     )
 
     assert product.id == "1"
-    assert product.product_name == "Leite"
-    assert product.company_name == "Parmalat"
-    assert product.manufacturing_date == "10/01/2024"
+    assert product.product_name == "aptamil"
+    assert product.company_name == "Danone"
+    assert product.manufacturing_date == "29/11/2023"
     assert product.expiration_date == "10/02/2024"
     assert product.serial_number == "1821318274"
     assert (
         product.storage_instructions
-        == "Guardar em lugar ventilado e longe do sol"
+        == "Fórmula infantil de seguimento para lactentes"
     )
