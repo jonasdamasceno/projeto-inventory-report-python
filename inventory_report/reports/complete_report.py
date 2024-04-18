987297b82537cc1baf9d08f4befbcd3037250195
jonas <jonasmichael2021@outlook.com>
2024-04-17 22:26:44 -0300

    fix requisito 9

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,13 +1,17 @@
+"""Function printing python version."""
+
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
+    """Class representing a person"""
+
     def generate(self) -> str:
         simple_report = super().generate()
         complete_report = "Stocked products by company:\n"
         complete_report += "\n".join(
             [
-                f"{company}: {value}"
+                f"- {company}: {value}"
                 for company, value in self.companies_inventories.items()
             ]
         )
